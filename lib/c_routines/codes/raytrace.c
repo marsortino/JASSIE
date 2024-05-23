61322ca10657991a98cd4952193176afd93e04b9
marsortino <118361726+marsortino@users.noreply.github.com>
2024-05-23 16:16:21 +0200

    fixed output names, added distance sortings
    Overhauled C code: 
    >> fixed output names in order to avoid race conditions.
    >> introduced new functions. Now it sorts the spheres by distance wrt to blob i-th and write their index on ad hoc tmp output file.

--- lib/c_routines/codes/raytrace.c
+++ lib/c_routines/codes/raytrace.c
@@ -16,13 +16,23 @@ typedef struct{
 	double r;
 } block;
 
+typedef struct{
+	int index;
+	double distance;
+} block_distance;
+
 double distance_between_line_point(vec p, vec q, vec r);
 vec normal_vec(vec source, vec block_i);
 double interacting_region(vec source, vec point_on_plane, vec normal);
-long double chord(double radius, double distance);
-double raytracing(int block_num, int max, block* blocklist, vec source, int **indices, int* psphere_counter, int* psphere_counter_max);
-void update_indices(int row, int col, int sphere_index, int **indices);
-char* file_name(char* filepath);
+double chord(double radius, double distance);
+double raytracing(int i, int max, block* blocklist, vec source, int* psphere_counter, int* psphere_max_counter, 	block_distance *distance_sorter);
+void preset_array(int sphere_counter, int sphere_index, double distance, block_distance *distance_array, double* pdistance_max);
+void update_indices(int **indices, int row, int number_of_spheres, block_distance *distance_array);
+char* get_file_name(char* filepath);
+double compute_distance(block block_j, block block_i);
+void sorting_distance(block_distance* distance_array, int sphere_index, long double distance, int max_index);
+void check_duplicates(block_distance* distance_sorted, int size);
+
 
 int main(int argc, char *argv[]){
 	
@@ -36,11 +46,11 @@ int main(int argc, char *argv[]){
 	double total_t;
 	start_t = clock();
 	vec source;
-	int i = 0, j, max, sphere_counter, sphere_counter_max = 0, *psphere_counter, *psphere_counter_max;
-	FILE *list;
+	int i = 0, j, max, sphere_counter, sphere_counter_max = 0, *psphere_counter, *psphere_counter_max, strlenght;
+	FILE *list_raytrace, *list_index;
 	block *blocklist;
 	long double *raytrace;
-
+	block_distance *distance_array;
 
 	source.x = strtod(argv[1], NULL);
 	source.y = strtod(argv[2], NULL);
@@ -51,69 +61,75 @@ int main(int argc, char *argv[]){
 	strcpy(file_path, argv[4]);
 
 
-	puts(file_path);
-	list = fopen(file_path, "r");
+	list_raytrace = fopen(file_path, "r");
 
-	//list = fopen("../../tmp_files/block_list_value.txt", "r");
-	fscanf(list, "%i", &max);	
+	fscanf(list_raytrace, "%i", &max);	
 
 	blocklist = malloc(max*sizeof(block));
 
-	while(!feof(list)){
+	while(!feof(list_raytrace)){
 
-		fscanf(list, "%le %le %le %le", &blocklist[i].center.x, &blocklist[i].center.y, &blocklist[i].center.z, &blocklist[i].r); 
+		fscanf(list_raytrace, "%le %le %le %le", &blocklist[i].center.x, &blocklist[i].center.y, &blocklist[i].center.z, &blocklist[i].r); 
 		i++;
 	}
 
-	fclose(list);
+	fclose(list_raytrace);
 
 	raytrace = malloc(max*sizeof(long double));
-	*file_path = file_name(file_path);
-	// We define a matrix n^2 made by -1. Each row represents a blob. The code computes which blob are in between the i-th blob and point P.
-	// When this happens, the code updates the value in that row with the index of the blob that lies in between.
 
-	int **indices = (int **)malloc(max * sizeof(int *));
-	for(i = 0; i < max; i++){
-		indices[i] = (int *)malloc(max * sizeof(int));
-		memset(indices[i], -1, max * sizeof(int));
-	}
+	char *filename = get_file_name(file_path);
+	int str_lenght =  strlen(filename);
 
+	char *outputname_rt = (char *)malloc(str_lenght+29 * sizeof(char)); // +15 because tmp_raytrace_o_ are 14 chars.
+	strcpy(outputname_rt, "lib/tmp_files/tmp_raytrace_o_");
+	strcat(outputname_rt, filename);
 
-	list = fopen("lib/tmp_files/tmp_raytrace_output.txt", "w");
+	char *outputname_indx = (char *)malloc(str_lenght+26 *sizeof(char));
+	strcpy(outputname_indx, "lib/tmp_files/tmp_indices_");
+	strcat(outputname_indx, filename);
+	
+	distance_array = malloc(max*sizeof(block_distance));
 
+	list_raytrace = fopen(outputname_rt, "w");
+	list_index = fopen(outputname_indx, "w");
 	psphere_counter = &sphere_counter;
 	psphere_counter_max = &sphere_counter_max;
 
+
+
 	for(i=0; i<max; i++){
 		sphere_counter = 0;
-		raytrace[i] += raytracing(i, max, blocklist, source, indices, psphere_counter, psphere_counter_max);
-
-		fprintf(list, "%0.15Le\n", raytrace[i]);
-
-		//printf("block [%d] raytrace value: %Le\n", i, raytrace[i]);
+		raytrace[i] += raytracing(i, max, blocklist, source, psphere_counter, psphere_counter_max, distance_array);
+		for(j = 0; j<sphere_counter-1; j++){
+			fprintf(list_index, "%d, ", distance_array[j].index);
+		}
+		fprintf(list_index, "%d\n", distance_array[sphere_counter-1].index);
+		fprintf(list_raytrace, "%0.15Le\n", raytrace[i]);
 	}
+	free(distance_array);
 	end_t = clock();
 
-	fclose(list);
+	fclose(list_raytrace);
+	fclose(list_index);
 	free(blocklist);
 	free(raytrace);
-	free(indices);
+
 	total_t = (double)(end_t-start_t)/CLOCKS_PER_SEC;
 	printf("Raytrace total time: %f\n", total_t);	
 	return 0;
 }
 
-double raytracing(int i, int max, block* blocklist, vec source, int **indices, int* psphere_counter, int* psphere_max_counter){
+double raytracing(int i, int max, block* blocklist, vec source, int* psphere_counter, int* psphere_max_counter, 	block_distance *distance_sorter){
 	/**/
 	int j;
 	long double raypath = 0, value;
 	int obs_sign, block_j_sgn1, block_j_sgn2;
 	vec normal;
-
+	double distance, distance_max = 0, *pdistance_max = &distance_max;
 
 	normal = normal_vec(source, blocklist[i].center);
-
 	obs_sign = interacting_region(source, blocklist[i].center, normal);
+
 
 	for(j=0; j < max; j++){
 
@@ -126,20 +142,24 @@ double raytracing(int i, int max, block* blocklist, vec source, int **indices, i
 
 				if (value == 0){
 					raypath+=blocklist[j].r;
-					update_indices(i,  *psphere_counter, j, indices);
+					distance = compute_distance(blocklist[j], blocklist[i]);
+					preset_array(*psphere_counter, j , distance, distance_sorter, pdistance_max);
 					(*psphere_counter)++;
+
 				}
 				else if ((value > 0) && (value < blocklist[j].r)){
 					raypath += chord(blocklist[j].r, value);
-					update_indices(i,  *psphere_counter, j, indices);
+					distance = compute_distance(blocklist[j], blocklist[i]);
+					preset_array(*psphere_counter, j , distance, distance_sorter, pdistance_max);
 					(*psphere_counter)++;
+
 				}
 			}					
 		}
 	}
-	if ((*psphere_counter) > (*psphere_max_counter)) (*psphere_max_counter) = (*psphere_counter);
 
-		
+	if ((*psphere_counter) > (*psphere_max_counter)) (*psphere_max_counter) = (*psphere_counter);		
+
 	return raypath;
 }  /* raytracing */
 
@@ -171,7 +191,7 @@ double interacting_region(vec source, vec point_on_plane, vec normal){
 	return distance;
 }   /* interacting_region */
 
-long double chord(double radius, double distance){
+double chord(double radius, double distance){
 	/* Computes the chord of the blob determined by the intersection via the blob and the rayline*/
 	return 2*sqrt(2*radius*distance-distance*distance);
 }  /* chord */
@@ -209,15 +229,55 @@ double distance_between_line_point(vec p, vec q, vec r){
 }
 
 
-void update_indices(int row, int col, int sphere_index, int **indices){
+void preset_array(int sphere_counter, int sphere_index, double distance, block_distance *distance_array, double* pdistance_max){
 	/*
-	Updates the indices matrix. 
+	Updates the indices matrix, sorting them by distance from the blob i-th.
 	*/
-	indices[row][col] = sphere_index;
+	int test_variable;
+
+	if(sphere_counter < 2){
+		if(sphere_counter == 0){
+ 		*pdistance_max = distance;
+		// Initialize the sorting distance array:
+		distance_array[sphere_counter].index = sphere_index;
+		distance_array[sphere_counter].distance = distance;
+		}
+		else{
+			if (distance > *pdistance_max){
+				*pdistance_max = distance;
+				// The sorting array is setted
+				distance_array[sphere_counter].index = sphere_index;
+				distance_array[sphere_counter].distance = distance;
+			} 
+			else{
+				// Switching the two index position and setting the array:
+				distance_array[sphere_counter].index = distance_array[0].index;
+				distance_array[sphere_counter].distance = distance_array[0].distance;
+				distance_array[0].index = sphere_index;
+				distance_array[0].distance = distance;
+			}
+		}
+	// The sorting array is now setted, with 0 having the lower distance from the blob i-th.
+		return;
+	}
+
+	else{
+		sorting_distance(distance_array, sphere_index, distance, sphere_counter);
+		return;
+	}
+	// For debugging purposes--
+	scanf("%d", &test_variable);
 }
 
-char* file_name(char* filepath){
+
+char* get_file_name(char* filepath){
+	/*
+	Detects the correct file name and define the new output name.
+	*/
 	char *filename;
+
+	size_t str_lenght;
+
 
     // Find the last occurrence of '/'
     filename = strrchr(filepath, '/');
@@ -231,8 +291,83 @@ char* file_name(char* filepath){
     }
 
     // Print the filename
-    printf("Filename: %s\n", filename);
+	str_lenght =  strlen(filename);
+	filename = filename + 15; // we are adding 'lib/tmp_files/tmp_raytrace_o_' ie. 29 chars
+	// char *outputname_rt = (char *)malloc(str_lenght+15 * sizeof(char)); // +15 because tmp_raytrace_o_ are 14 chars.
+	// strcpy(outputname_rt, "lib/tmp_files/tmp_raytrace_o_");
+	// strcat(outputname_rt, filename);
 
-    return filename;
+	// char *outputname_index = (char *)malloc(str_lenght);
+	// strcpy(outputname, "lib/tmp_files/tmp_indices_");
+    // return outputname;
+	return filename;
+}
 
+double compute_distance(block block_j, block block_i){
+	/*
+	Computes the distance between two blocks.
+	*/
+	double x,y,z, distance;
+	x =  block_j.center.x - block_i.center.x;
+	y =  block_j.center.y - block_i.center.y;
+	z =  block_j.center.z - block_i.center.z;
+	distance = sqrt(pow(x, 2)+pow(y,2)+pow(z,2));
+
+	return sqrt(pow(x, 2)+pow(y,2)+pow(z,2));
+}
+
+void sorting_distance(block_distance* distance_array, int sphere_index, long double distance, int max_index){
+	/* 
+	Sorts the given array by distance and returns the place in
+	*/
+	int flag = 0, i=0, j;
+		// Check the condition for the first element in the array:
+		if (distance_array[0].distance > distance){
+			for (j=max_index+1; j>0; j--){
+				distance_array[j].distance = distance_array[j-1].distance;
+				distance_array[j].index = distance_array[j-1].index;
+			}
+			distance_array[0].distance = distance;
+			distance_array[0].index = sphere_index;
+			flag ++;
+		}
+		// Check the condition for the last element in the array;
+		if (distance_array[max_index-1].distance < distance){
+			distance_array[max_index].distance = distance;
+			distance_array[max_index].index = sphere_index;
+			flag++;
+		}
+	while(flag == 0){
+		if (distance_array[i].distance <= distance && distance <= distance_array[i+1].distance){
+			for(j=max_index+1; j>i; j--){
+				distance_array[j+1].distance = distance_array[j].distance;
+				distance_array[j+1].index = distance_array[j].index;
+			}
+			distance_array[i+1].distance = distance;
+			distance_array[i+1].index = sphere_index;
+			flag++;
+			return;
+		}
+		else{
+			i++;
+			if (i>max_index){
+				printf("Something went wrong in sorting distance. I'm not supposed to be here...\n");
+				flag++;
+			}
+		}
+	}
+	return;
+
+}
+
+void check_duplicates(block_distance* distance_sorted, int size){
+	/* Only for debugging purposes*/
+
+	for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (distance_sorted[i].index == distance_sorted[j].index) { // check if current element is equal to another
+                break;
+			}
+		}
+	}
 }
